dd2eb0ee06e6b5191d163a50a6f153601734d981
Karan Kumar <officialkaran39@gmail.com>
2024-05-11 13:30:50 +0530

    once again push

--- Lec1ModularArithmetic.cpp
+++ Lec1ModularArithmetic.cpp
@@ -16,7 +16,23 @@ int main(){
     long long fact =1;
     for (int i = 2; i <=n; i++)
     {
-        fact = (fact * i) % M;
-    }
-    cout<< fact;
+        fact = (fact * i) % M;            // here modulo is used 
+                                          // becoz when finding the factorial of the greater number it gives negative big number , 
+                                          // simply means long long cannot hold that big number. 
+                                          // thats why modulo come into picture , like A % M result is always smaller than M. 
 }
+    cout<< fact;           
+}
+
+/*
+Given a number N. Print its factorial.
+constraints
+1<= N <= 100
+
+Print answer modulo M
+where M = 10^9 + 7   
+ --> here we sometimes find this M value, it is very close to integer maximum,
+     so for storing the big value inside the int range then , this value of 
+    Modulo is used.
+    And also it is a prime number thats why we can easily find the multiplicative inverse 
+*/
